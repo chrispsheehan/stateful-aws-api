a4924eb1f78b609cb84b46770aee7056b281ae71
chrispsheehan <chrispsheehan@gmail.com>
2024-04-24 21:10:11 +0100

    tf init working

--- tf/data.tf
+++ tf/data.tf
@@ -27,3 +27,16 @@ data "aws_iam_policy_document" "api_lambda_policy" {
     ]
   }
 }
+
+data "aws_iam_policy_document" "api_gw" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    effect    = "Allow"
+    actions   = ["execute-api:Invoke"]
+    resources = ["${aws_api_gateway_rest_api.this.execution_arn}/*/*/*"]
+  }
+}
--- tf/main.tf
+++ tf/main.tf
@@ -1,32 +1,8 @@
-resource "aws_iam_policy" "api_lambda_policy" {
-  name        = "${local.lambda_name}_api_policy"
-  description = "IAM policy to allow Lambda function to read parameter from SSM"
-
-  policy = data.aws_iam_policy_document.api_lambda_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "api_lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_execution_role.name
-  policy_arn = aws_iam_policy.api_lambda_policy.arn
-}
-
-resource "aws_iam_role" "lambda_execution_role" {
-  name               = "${local.lambda_name}_lambda_execution_role"
-  assume_role_policy = data.aws_iam_policy_document.api_lambda_assume_role.json
-}
-
-resource "aws_lambda_permission" "this" {
-  statement_id  = "${local.lambda_name}_AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-}
-
-
 resource "aws_lambda_function" "lambda" {
   filename      = var.lambda_zip_path
   function_name = local.lambda_name
   role          = aws_iam_role.lambda_execution_role.arn
+  
   handler       = "app.handler"
   runtime       = "nodejs20.x"
   timeout       = 10
@@ -64,26 +40,97 @@ resource "aws_dynamodb_table" "tasks" {
   }
 }
 
-resource "aws_apigatewayv2_api" "this" {
-  name          = "${local.lambda_name}-APIGateway"
-  protocol_type = "HTTP"
-  target        = aws_lambda_function.lambda.invoke_arn
+resource "aws_s3_bucket" "lambda-bucket" {
+  bucket = local.lambda_bucket
 }
 
-resource "aws_apigatewayv2_integration" "this" {
-  api_id           = aws_apigatewayv2_api.this.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.lambda.invoke_arn
+resource "aws_s3_object" "lambda_zip" {
+  bucket        = aws_s3_bucket.lambda-bucket.id
+  key           = basename(var.lambda_zip_path)
+  source        = var.lambda_zip_path
+  force_destroy = true
 }
 
-resource "aws_apigatewayv2_route" "this" {
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
+resource "aws_iam_role" "lambda_execution_role" {
+  name               = "${local.lambda_name}_lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.api_lambda_assume_role.json
 }
 
-resource "aws_apigatewayv2_stage" "this" {
-  api_id      = aws_apigatewayv2_api.this.id
-  name        = "lambda"
-  auto_deploy = false
+resource "aws_iam_policy" "lambda_execution_iam_policy" {
+  name   = "${local.lambda_name}_lambda_execution_iam"
+  policy = data.aws_iam_policy_document.api_lambda_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  depends_on = [ aws_dynamodb_table.tasks ]
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_execution_iam_policy.arn
+}
+
+resource "aws_lambda_permission" "this" {
+  statement_id  = "${local.lambda_name}-AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_rest_api" "this" {
+  name        = "${local.lambda_name}-APIGateway"
+  description = "${local.lambda_name} API Gateway"
+}
+
+resource "aws_api_gateway_resource" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "this" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "this" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.this.id
+  http_method             = aws_api_gateway_method.this.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "this" {
+  depends_on = [
+    aws_api_gateway_integration.this,
+    aws_api_gateway_method.this
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "this" {
+  deployment_id = aws_api_gateway_deployment.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  stage_name    = "lambda"
+}
+
+resource "aws_api_gateway_method_settings" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name  = aws_api_gateway_stage.this.stage_name
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = false
+  }
+}
+
+resource "aws_api_gateway_rest_api_policy" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  policy      = data.aws_iam_policy_document.api_gw.json
 }
