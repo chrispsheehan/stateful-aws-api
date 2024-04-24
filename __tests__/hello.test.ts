be558b4849f12ef3aa74a1a8faf01f22eb890090
chrispsheehan <chrispsheehan@gmail.com>
2024-04-24 11:30:28 +0100

    mvp hello test

---
+++ __tests__/hello.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect, beforeAll } from "@jest/globals"
+import request, { Response } from 'supertest';
+
+let baseUrl: string;
+let response: Response;
+
+beforeAll(() => {
+  baseUrl = process.env.BASE_URL || "";
+});
+
+describe('/hello', () =>{
+  test('is 200', async () =>{
+    response = await request(baseUrl).get('/hello');
+    expect(response.status).toBe(200);
+  })
+})
