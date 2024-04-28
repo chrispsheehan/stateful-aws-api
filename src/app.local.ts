422b78bf222d3e35113c6175ad229e9a2ea2387f
chrispsheehan <chrispsheehan@gmail.com>
2024-04-28 14:19:55 +0100

    logging

--- src/app.local.ts
+++ src/app.local.ts
@@ -1,5 +1,12 @@
 import app from './app'
+import { Request } from 'express';
 const port = process.env.API_PORT
 
 app.listen(port)
+
+app.use((req: Request, _, next) => { 
+    console.log(`Params: ${JSON.stringify(req.params)}`); // log out requests
+    console.log(`Headers: ${JSON.stringify(req.headers)}`);
+});
+
 console.log(`LOCAL app listening on http://localhost:${port}`)
